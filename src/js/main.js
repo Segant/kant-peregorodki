e71a6ba471e2af7042b17e9854148117af757628
Rail321 <71922301+Rail321@users.noreply.github.com>
2020-12-22 19:41:53 +0300

    ready navigation menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,26 @@
 window.onload = () => {
 
+	//Menu transforming in scrool
+
+	navScroll();
+
+	function navScroll() {
+		if ( $(window).scrollTop() == 0) {
+			$("#header").removeClass( "header-scroll" );
+		} else {
+			$("#header").addClass( "header-scroll");
+		}
+	}
+
+	$(window).scroll( () => {
+		navScroll();
+	});
+
+	//Burger
+
+	$("#burger").click( () => {
+		$("#burger").toggleClass("header__burger-active");
+		$("#nav").toggleClass("header__menu-active");
+	});
 
 };
