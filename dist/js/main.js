9031283c24deb59748a5bb31f85e814c910fd41c
Segant <segant333@gmail.com>
2020-12-26 23:30:19 +0500

    meeting notes

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,18 +27,33 @@ $( document ).ready( () => {
 		$('body').toggleClass('overflow-hidden');
 	});
 
+	const paginationTextDesktop = [
+		'1 точка',
+		'2 точка',
+		'3 точка',
+		'4 точка',
+		'5 точка',
+		'6 точка',
+	];
+
+	
 	$('.calculus__slider').slick({
 		infinite: false,
 		dots: true,
 		appendDots: '.calculus__dots',
 		appendArrows: '.calculus__buttons',
 		prevArrow:"<button type='button' class='button lines animation slick-prev pull-left'><span>Назад</span></button>",
-      nextArrow:"<button type='button' class='button lines animation slick-next pull-right'><span>Далее</span></button>"
+		nextArrow:"<button type='button' class='button lines animation slick-next pull-right'><span>Далее</span></button>",
+		customPaging: function(slick,i) {
+			return `<a>${paginationTextDesktop[i]}</a>`;
+		}
 	});
 
+
+
 	$('.realization--item__wrapper').click( function() {
-		text = $(this).children('.realization--value').text();
-		$('.calculus--preview__realization--value').text( text);
+		const text = $(this).children('.realization--value').text();
+		$('.calculus--preview__realization--value').text(text);
 	});
 
 });
